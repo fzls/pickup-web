abe7ade9cdc0f2a6247bae6cd0a20db28e948238
Chen Ji <fzls.zju@gmail.com>
2016-11-28 23:14:05 +0800

    Initial commit

---
+++ _ide_helper_fluent.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 风之凌殇
+ * Date: 6/25/2016
+ * Time: 10:22 AM
+ */
+namespace Illuminate\Support;
+
+/**
+ * @method Fluent after($column) Place the column "after" another column (MySQL Only)
+ * @method Fluent change() Updating Column Attributes, need  add the doctrine/dbal dependency
+ * @method Fluent comment($comment) Add a comment to a column
+ * @method Fluent default($value) Specify a "default" value for the column
+ * @method Fluent first() Place the column "first" in the table (MySQL Only)
+ * @method Fluent index() Add a basic index.
+ * @method Fluent nullable() Allow NULL values to be inserted into the column
+ * @method Fluent on($value)
+ * @method Fluent onDelete($value)
+ * @method Fluent onUpdate($value)
+ * @method Fluent primary($column) Add a primary key. or Add composite keys.
+ * @method Fluent references($value)
+ * @method Fluent storedAs($expression) Create a stored generated column (MySQL Only)
+ * @method Fluent unique() unique index
+ * @method Fluent unsigned() Set integer columns to UNSIGNED
+ * @method Fluent virtualAs($expression) <Create a virtual generated column (MySQL Only)>
+ */
+class Fluent {}
