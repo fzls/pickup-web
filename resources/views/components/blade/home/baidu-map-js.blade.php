6d5fdb68fc2f491d39bf40cddb737261b3c03205
Chen Ji <fzls.zju@gmail.com>
2016-12-14 00:18:02 +0800

    fix baidu map https

--- resources/views/components/blade/home/baidu-map-js.blade.php
+++ resources/views/components/blade/home/baidu-map-js.blade.php
@@ -1,2 +1,5 @@
-<script type="text/javascript" src="https://api.map.baidu.com/api?v=2.0&ak=pcXKABjQgrWR7frUWMApZVZ69nGYmHFg"></script>
+{{--<script type="text/javascript" src="https://api.map.baidu.com/api?v=2.0&ak=pcXKABjQgrWR7frUWMApZVZ69nGYmHFg"></script>--}}
+<script>
+    (function(){ window.BMap_loadScriptTime = (new Date).getTime(); document.write('<script type="text/javascript" src="https://api.map.baidu.com/getscript?v=2.0&ak=pcXKABjQgrWR7frUWMApZVZ69nGYmHFg&services=&t=20161208141700"></script>');})();
+</script>
 <script src="{{asset('/js/util/initialize-baidu-map.js')}}"></script>
