4f6de044c0732e27abbaba473add0e606f389ad1
Chen Ji <fzls.zju@gmail.com>
2016-12-14 00:46:03 +0800

    修改百度地图的https

--- resources/views/components/blade/home/baidu-map-js.blade.php
+++ resources/views/components/blade/home/baidu-map-js.blade.php
@@ -1,5 +1,5 @@
-{{--<script type="text/javascript" src="https://api.map.baidu.com/api?v=2.0&ak=pcXKABjQgrWR7frUWMApZVZ69nGYmHFg"></script>--}}
-<script>
-    (function(){ window.BMap_loadScriptTime = (new Date).getTime(); document.write('<script type="text/javascript" src="https://api.map.baidu.com/getscript?v=2.0&ak=pcXKABjQgrWR7frUWMApZVZ69nGYmHFg&services=&t=20161208141700"></script>');})();
-</script>
+<script type="text/javascript" src="https://api.map.baidu.com/api?v=2.0&ak=pcXKABjQgrWR7frUWMApZVZ69nGYmHFg&s=1"></script>
+{{--<script>--}}
+    {{--(function(){ window.BMap_loadScriptTime = (new Date).getTime(); document.write('<script type="text/javascript" src="https://api.map.baidu.com/getscript?v=2.0&ak=pcXKABjQgrWR7frUWMApZVZ69nGYmHFg&services=&t=20161208141700"></script>');})();--}}
+{{--</script>--}}
 <script src="{{asset('/js/util/initialize-baidu-map.js')}}"></script>
